11c3c3367da89c3b6966f36693afb905387cb434
C-Florian <florianchereau@hotmail.fr>
2025-07-30 21:19:50 +0200

    Task 4 - Define variables

---
+++ python_variable_annotations/4-define_variables.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+"""This module defines and annotates various variables."""
+
+a: int = 1
+pi: float = 3.14
+i_understand_annotations: bool = True
+school: str = "Holberton"
+
+# Printing the variables and their types
+print("a is a {} with a value of {}".format(type(a), a))
+print("pi is a {} with a value of {}".format(type(pi), pi))
+print("i_understand_annotations is a {} with a value of {}".format(type(i_understand_annotations), i_understand_annotations))
+print("school is a {} with a value of {}".format(type(school), school))
