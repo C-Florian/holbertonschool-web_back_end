6c376ad5b785dbe6b0049206ce9a8b3812e58e8f
C-Florian <florianchereau@hotmail.fr>
2025-08-22 15:11:35 +0200

    Task 5: Implement Building with abstract-like enforcement for evacuationWarningMessage

---
+++ ES6_classes/5-building.js
@@ -0,0 +1,18 @@
+export default class Building {
+  constructor(sqft) {
+    if (typeof sqft !== 'number') {
+      throw new TypeError('Square feet must be a number');
+    }
+
+    // Enforce "abstract" requirement on subclasses
+    if (new.target !== Building && this.evacuationWarningMessage === undefined) {
+      throw new Error('Class extending Building must override evacuationWarningMessage');
+    }
+
+    this._sqft = sqft;
+  }
+
+  get sqft() {
+    return this._sqft;
+  }
+}
