1420a2beeedc4ff989a88cd40cf9e18cfd681a83
C-Florian <florianchereau@hotmail.fr>
2025-09-05 16:31:17 +0200

    NodeJS Basics - Task 5: HTTP server with /students using async CSV reader

---
+++ Node_JS_basic/5-http.js
@@ -0,0 +1,40 @@
+// 5-http.js
+const http = require('http');
+const countStudents = require('./3-read_file_async');
+
+const DB_PATH = process.argv[2];
+
+const app = http.createServer((req, res) => {
+  const { url } = req;
+
+  if (url === '/') {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.end('Hello Holberton School!');
+    return;
+  }
+
+  if (url === '/students') {
+    res.writeHead(200, { 'Content-Type': 'text/plain' });
+    res.write('This is the list of our students\n');
+    countStudents(DB_PATH)
+      .then(({ fields }) => {
+        const lines = [];
+        for (const field of Object.keys(fields)) {
+          lines.push(`Number of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}`);
+        }
+        res.end(lines.join('\n'));
+      })
+      .catch(() => {
+        res.end('Cannot load the database');
+      });
+    return;
+  }
+
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello Holberton School!');
+});
+
+app.listen(1245);
+
+module.exports = app;
+
